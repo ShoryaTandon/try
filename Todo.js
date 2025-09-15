181e62f516cf10088487ecbc590b64c2306fec9f
Shorya <Shoryatandon874@gmail.com>
2025-09-15 14:29:52 +0530

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,32 @@
+let btn =document.querySelector("Button");
+function addTask(){
+     let inp =document.querySelector("input");
+     let task =inp.value;
+     inp.value="";
+
+     let li =document.createElement("li");
+     let ul =document.querySelector("ul")
+
+     let cb=document.createElement("input")
+     cb.type="checkbox";
+
+     let delbtn =document.createElement("button");
+     delbtn.innerText="delete";
+
+     ul.append(li);
+     li.innerText=task;
+     
+
+     li.append(cb);
+     li.append(delbtn);
+
+   
+}
+
+// function delTask(){
+//     let li=delbtn.parentElement;
+//     li.remove();
+// }
+// delbtn.addEventListener("click",delTASK);
+
+btn.addEventListener("click",addTask);
